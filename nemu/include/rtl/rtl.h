9fb3347e1b9ddfd94763082fb93205abc75baf83
tracer-ics2019 <tracer@njuics.org>
2024-10-04 20:21:29 +0800

    >  compile U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:21:29 up 10:54,  1 user,  load average: 0.25, 0.16, 0.11 846b3e0e29b6942ab2bacbcbd440560f4d32dfdd

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -138,13 +138,12 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   // TODO();
-  int32_t temp=*src1;
-  if(width==1)
-    *dest=temp<<24>>24;
-  else if(width==2)
-    *dest=temp<<16>>16;
-  else
-    *dest=temp;
+  switch (width) {
+    case 4: *(int32_t*)dest = *(int32_t*)src1; return;
+    case 1: *(int32_t*)dest = *(int8_t*)src1; return;
+    case 2: *(int32_t*)dest = *(int16_t*)src1; return;
+    default: assert(0);
+  }
 }
 
 static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
