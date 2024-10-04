c06be4a1dd4b7e05661ce30ea43590acd990e7a7
tracer-ics2019 <tracer@njuics.org>
2024-10-04 19:12:48 +0800

    >  run U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:12:48 up  9:45,  1 user,  load average: 0.11, 0.14, 0.12 5edaa22b9e40c460f03596bc2c9cd06a757bd715

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,110 +3,19 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-#define MAX_SIZE 4096
-
-void puts_helper(const char* string,int ptr){
-  int i;
-  if(ptr<0){
-    for(i=0;string[i];++i)_putc(string[i]);
-  }else{
-    for(i=0;i<ptr;++i)_putc(string[i]);
-  }
-}
-
 int printf(const char *fmt, ...) {
-  va_list ap;
-  va_start(ap,fmt);
-  char buffer[MAX_SIZE];
-  int ptr = vsprintf(buffer,fmt,ap);
-  puts_helper(buffer,ptr);
-  va_end(ap);
   return 0;
-  return 0;
-}
-
-int itoa_helper(char *out, int ptr, uint32_t valint, int base){
-  assert(valint>=0);
-  int stack[128],sptr=0;
-  if(valint==0)out[ptr++]='0';
-  else{
-    for(;valint;stack[sptr++]=valint%base,valint=valint/base);
-    for(--sptr;sptr>=0;--sptr){
-      if(stack[sptr]<10)out[ptr++]=stack[sptr]+'0';
-      else if(stack[sptr]<16)out[ptr++]=stack[sptr]-10+'a';
-      else{
-        assert(0);
-      }
-    }
-  }
-  return ptr;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  int ptr=0;
-  for(;*fmt;++fmt){
-    if(*fmt!='%')out[ptr++]=*fmt;
-    else{
-      switch(*(++fmt)){
-        case 'c':{
-          char c = va_arg(ap,int);
-          out[ptr++]=c;
-          break;
-        }
-        case 'd':{
-          int valint=va_arg(ap,int);
-          if(valint<0){
-            out[ptr++]='-';
-            valint=-valint;
-          }
-          ptr = itoa_helper(out,ptr,valint,10);
-          break;
-        }
-        case 'x':{
-          uint32_t valint=va_arg(ap,uint32_t);
-          ptr = itoa_helper(out,ptr,valint,16);
-          break;
-        }
-        case 's':{
-          const char* valstr=va_arg(ap,char*);
-          for(;*valstr;out[ptr++]=*valstr++);
-          break;
-        }
-        case 'f':{
-          float valflt=va_arg(ap,float);
-          if(valflt<0){
-            out[ptr++]='-';
-            valflt=-valflt;
-          }
-          int tmpint=(int)valflt;
-          int tmpflt=(int)(10000*(valflt-tmpint));
-          ptr=itoa_helper(out,ptr,tmpint,10);
-          out[ptr++]='.';
-          ptr=itoa_helper(out,ptr,tmpflt,10);
-          break;
-        }
-        default:{
-          out[ptr++]='%';
-          out[ptr++]=*fmt;
-          break;
-        }
-      }
-    }
-  }
-  out[ptr]=0;
-  return ptr;
+  return 0;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  va_list ap;
-  va_start(ap,fmt);
-  int ptr=vsprintf(out,fmt,ap);
-  va_end(ap);
-  return ptr;
+  return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-
   return 0;
 }
 
