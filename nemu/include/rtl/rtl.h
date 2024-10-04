2adf972cfbad1a631c18801fb6dac9a886026b66
tracer-ics2019 <tracer@njuics.org>
2024-10-04 19:34:38 +0800

    >  compile U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:34:38 up 10:07,  1 user,  load average: 0.24, 0.24, 0.16 9d7055bee269dabff29f8c12bc238da54ed3d960

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -132,12 +132,14 @@ void interpret_rtl_exit(int state, vaddr_t halt_pc, uint32_t halt_ret);
 
 static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- ~src1
-  TODO();
+  // TODO();
+  *dest = ~(*src1);
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  // TODO();
+  *dest = (*src1 << (32 - width * 8)) >> (32 - width * 8);
 }
 
 static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
