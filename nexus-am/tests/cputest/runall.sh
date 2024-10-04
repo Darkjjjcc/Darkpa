f2bfec83a188596e95c7b1ed2b140bf680480388
tracer-ics2019 <tracer@njuics.org>
2024-10-04 15:02:50 +0800

    >  run U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:02:50 up  5:35,  1 user,  load average: 0.00, 0.03, 0.05 3ecf554bf31c13a170fb48a0665caae563a65a30

--- nexus-am/tests/cputest/runall.sh
+++ nexus-am/tests/cputest/runall.sh
@@ -1,1 +1,32 @@
-make ALL=dummy run
+#!/bin/bash
+
+# 1. 定义文件所在的文件夹
+FILE_DIR="./tests"  # 文件所在的文件夹
+
+# 2. 定义 Makefile 所在的文件夹
+MAKE_DIR=""  # Makefile 所在的目录
+
+# 3. 遍历文件夹中的所有文件
+for file in "$FILE_DIR"/*; do
+    if [[ -f $file ]]; then
+        echo "Processing $file ..."
+        
+        # 4. 切换到 Makefile 所在的文件夹
+        pushd "$MAKE_DIR" > /dev/null
+        
+        # 5. 执行 make run，并将文件的路径传递给 Makefile
+        make run FILE="$file"
+        
+        # 6. 检查 make run 的结果
+        if [[ $? -ne 0 ]]; then
+            echo "Make run failed for $file"
+            popd > /dev/null
+            exit 1
+        fi
+        
+        # 7. 返回原来的工作目录
+        popd > /dev/null
+    fi
+done
+
+echo "All files processed successfully."
