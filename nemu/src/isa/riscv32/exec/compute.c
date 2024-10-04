94c808a1de1ab5d8856f718c2e132a81840de3dc
tracer-ics2019 <tracer@njuics.org>
2024-10-04 17:12:01 +0800

    >  compile U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:12:01 up  7:44,  1 user,  load average: 0.09, 0.14, 0.08 520c5dabd9ea8595bfc182a9d717cade09fbea7

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -24,7 +24,37 @@ make_EHelper(auipc) {
 
 // PA2.2 I-type instructions
 make_EHelper(I_ir_4) {
-  rtl_add(&id_dest->val, &id_src->val, &id_src2->val);
+  switch (decinfo.isa.instr.funct3) {
+    case 0b000: // addi
+      rtl_add(&id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    case 0b010: // slti
+      rtl_setrelop(RELOP_LT, &id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    case 0b011: // sltiu
+      rtl_setrelop(RELOP_LTU, &id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    case 0b100: // xori
+      rtl_xor(&id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    case 0b110: // ori
+      rtl_or(&id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    case 0b111: // andi
+      rtl_and(&id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    case 0b001: // slli
+      rtl_shl(&id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    case 0b101:
+      if((decinfo.isa.instr.funct7) == 0b0000000) // srli
+        rtl_shr(&id_dest->val, &id_src->val, &id_src2->val);
+      else // srai
+        rtl_sar(&id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    default:
+      assert(0);
+  }
   rtl_sr(id_dest->reg, &id_dest->val, 4);
   print_asm_template3(I_ir_4);
 }
