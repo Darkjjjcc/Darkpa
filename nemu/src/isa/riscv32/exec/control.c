fc5513e112a612948d15be5a0e9f8461c1ddef7c
tracer-ics2019 <tracer@njuics.org>
2024-10-04 14:14:03 +0800

    >  compile U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:14:03 up  4:46,  1 user,  load average: 0.23, 0.20, 0.14 7b13cafd3d0655c263c08383cc3b04cf1ba36f2

--- nemu/src/isa/riscv32/exec/control.c
+++ nemu/src/isa/riscv32/exec/control.c
@@ -1,1 +1,23 @@
 #include "cpu/exec.h"
+//pa2
+make_EHelper(jal){
+  uint32_t addr = cpu.pc + 4;
+  rtl_sr(id_dest->reg, &addr, 4);
+  // printf("control.c J:%s=0x%x\n",reg_name(id_dest->reg,4),reg_l(id_dest->reg));  // my test
+
+  rtl_add(&decinfo.jmp_pc, &cpu.pc, &id_src->val);
+  rtl_j(decinfo.jmp_pc);
+
+  print_asm_template2(jal);
+}
+
+make_EHelper(jalr){
+  uint32_t addr = cpu.pc + 4;
+  rtl_sr(id_dest->reg, &addr, 4);
+  decinfo.jmp_pc = (id_src->val+id_src2->val)&(~1);
+  rtl_j(decinfo.jmp_pc);
+
+  difftest_skip_dut(1, 2); //difftest
+
+  print_asm_template2(jalr);
+}
