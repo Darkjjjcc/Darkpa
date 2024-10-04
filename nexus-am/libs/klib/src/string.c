3bab3d755140d13c69e5590030ebe1f4eeebb0ac
tracer-ics2019 <tracer@njuics.org>
2024-10-04 20:29:42 +0800

    >  run U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:29:42 up 11:02,  1 user,  load average: 0.47, 0.32, 0.20 4589a8e3714040b02b07564e386f65de25a694ed

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -65,16 +65,16 @@ void* memcpy(void* out, const void* in, size_t n) {
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  const unsigned char *p1 = (const unsigned char *)s1;  
-  const unsigned char *p2 = (const unsigned char *)s2;  
+  // const unsigned char *p1 = (const unsigned char *)s1;  
+  // const unsigned char *p2 = (const unsigned char *)s2;  
 
-  while (n--) {
-      if (*p1 != *p2) {  
-          return (int)(*p1 - *p2);  
-      }  
-      p1++;  
-      p2++;  
-  }  
+  // while (n--) {
+  //     if (*p1 != *p2) {  
+  //         return (int)(*p1 - *p2);  
+  //     }  
+  //     p1++;  
+  //     p2++;  
+  // }  
   return 0;
 }
 
