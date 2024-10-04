e4c6736bd89ec7ed2cbce324c4b949357504d92b
tracer-ics2019 <tracer@njuics.org>
2024-10-04 19:07:44 +0800

    >  run U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:07:44 up  9:40,  1 user,  load average: 0.22, 0.16, 0.12 af75c05f23cfdf2084ba8cb7d74c855ee891047a

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -4,18 +4,81 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
+
   return 0;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+    char *str = out;
+    const char *p = fmt;
+    while (*p) {
+        if (*p != '%') {
+            *str++ = *p++;
+            continue;
+        }
+        p++; // Skip '%'
+        switch (*p) {
+            case 'c': {
+                char c = (char) va_arg(ap, int);
+                *str++ = c;
+                break;
+            }
+            case 'd':
+            case 'i': {
+                int i = va_arg(ap, int);
+                str += sprintf(str, "%d", i);
+                break;
+            }
+            case 'u': {
+                unsigned int u = va_arg(ap, unsigned int);
+                str += sprintf(str, "%u", u);
+                break;
+            }
+            case 'x':
+            case 'X': {
+                unsigned int x = va_arg(ap, unsigned int);
+                str += sprintf(str, "%x", x);
+                break;
+            }
+            case 's': {
+                const char *s = va_arg(ap, const char *);
+                while (*s) {
+                    *str++ = *s++;
+                }
+                break;
+            }
+            case 'p': {
+                void *ptr = va_arg(ap, void *);
+                str += sprintf(str, "%p", ptr);
+                break;
+            }
+            case '%': {
+                *str++ = '%';
+                break;
+            }
+            default: {
+                // Handle unknown format specifier
+                *str++ = '%';
+                *str++ = *p;
+                break;
+            }
+        }
+        p++;
+    }
+    *str = '\0';
+    return str - out;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+    va_list ap;
+    va_start(ap, fmt);
+    int ret = vsprintf(out, fmt, ap);
+    va_end(ap);
+    return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
+
   return 0;
 }
 
