01f145d95774a5eb771790ea8d6bdab12ff6ab8d
tracer-ics2019 <tracer@njuics.org>
2024-10-04 20:20:04 +0800

    >  compile U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:20:04 up 10:52,  1 user,  load average: 0.48, 0.18, 0.11 dc36957c6d4003aac23bbd144dbc95663a4aa3

--- nemu/src/isa/riscv32/exec/ldst.c
+++ nemu/src/isa/riscv32/exec/ldst.c
@@ -26,13 +26,13 @@ make_EHelper(st) {
 make_EHelper(lh){
   rtl_lm(&s0, &id_src->addr, decinfo.width);
   rtl_sext(&s1, &s0, decinfo.width);
-  rtl_sr(id_dest->reg,&s1,4);
+  rtl_sr(id_dest->reg,&s0,4);
   print_asm_template2(lh);
 }
 
 make_EHelper(lb){
   rtl_lm(&s0,&id_src->addr,decinfo.width);
   rtl_sext(&s1,&s0,decinfo.width);
-  rtl_sr(id_dest->reg,&s1,4);
+  rtl_sr(id_dest->reg,&s0,4);
   print_asm_template2(lb);
 }
