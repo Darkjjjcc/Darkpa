e93acf196f60697dbc7fcb6cb95c1a9994cd0a22
tracer-ics2019 <tracer@njuics.org>
2024-10-05 16:04:43 +0800

    >  run U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:04:43 up  6:42,  1 user,  load average: 0.12, 0.13, 0.04 6bde12006f14f63854d53a113b2c75e35bf2cf0d

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,123 +3,98 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-const char lower_dict[] = "0123456789abcdef";
-const char upper_dict[] = "0123456789ABCDEF";
-
-// 借鉴了 https://github.com/atgreen/FreeRTOS/blob/master/Demo/CORTEX_STM32F103_Primer_GCC/printf-stdarg.c
-static inline int _printc(char **out, char c) {
-  if (out) {
-    **out = c;
-    (*out)++;
-  } else {
-    _putc(c);
-  }
-  return 1;
-}
-
-static inline int _prints(char **out, const char *s) {
-  int cnt = 0;
-  while (*s != '\0') {
-    cnt += _printc(out, *(s++));
-  }
-  return cnt;
-}
-
-static inline int _printi(char **out, int num, int base, int use_upper, int sign) {
-  assert(base <= 16 && base >= 2);
-
-  const int buf_sz = 51;
-  char buf[buf_sz];
-  buf[buf_sz - 1] = '\0';
-  const char *dict = use_upper ? upper_dict : lower_dict;
-
-  if (num == 0) {
-    return _printc(out, dict[0]);
-  }
-
-  char *p = &buf[buf_sz - 1];
-  if (!sign) {
-    unsigned int n = *(unsigned int *)&num;
-    while (n != 0) {
-      p--;
-      *p = dict[n % base];
-      n /= base;
-    }
-  } else {
-    int is_neg = 0;
-    if (num < 0) {
-      num = -num;
-      is_neg = 1;
-    }
-
-    while (num != 0) {
-      p--;
-      *p = dict[num % base];
-      num /= base;
-    }
-
-    if (is_neg) {
-      p--;
-      *p = '-';
-    }
-  }
-
-  return _prints(out, p);
-}
-
-static inline int _print(char **out, const char *fmt, va_list ap) {
-  const char *p = fmt;
-  int cnt = 0;
-  while (*p) {
-    if (*p == '%') {
-      p++;
-      switch (*p) {
-        case 'd':
-          cnt += _printi(out, va_arg(ap, int), 10, 0, 1);
-          break;
-        case 'u':
-          cnt += _printi(out, va_arg(ap, int), 10, 0, 0);
-          break;
-        case 'x':
-          cnt += _printi(out, va_arg(ap, int), 16, 0, 0);
-          break;
-        case 'X':
-          cnt += _printi(out, va_arg(ap, int), 16, 1, 0);
-          break;
-        case 's':
-          cnt += _prints(out, va_arg(ap, char *));
-          break;
-        default:
-          break;
-      }
-    } else {
-      cnt += _printc(out, *p);
-    }
-    p++;
-  }
-  if (out)
-    **out = '\0';
-  return cnt;
-}
-
 int printf(const char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  int cnt = _print(NULL, fmt, ap);
-  va_end(ap);
-  return cnt;
+    //FIXME: fix the potential problem of overflow
+    char tmp[200];
+    va_list ap;
+    va_start(ap, fmt);
+    int count = vsprintf(tmp, fmt, ap);
+    for(int i = 0;tmp[i];i++){
+        _putc(tmp[i]);
+    }
+    va_end(ap);
+    return count;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return _print(&out, fmt, ap);
+    int count = 0, flag = 0, fmtnum = 0;
+    char tmpc[30], sflag;
+    char *tmp;
+    int inte;
+    int i = 0, j = 0, k;
+
+    for(;fmt[i];i++){
+        if(flag){
+            flag = 0;
+            if(fmt[i]>='0' && fmt[i]<='9'){
+                fmtnum *= 10;
+                fmtnum += fmt[i]-'0';
+                flag = 1;
+                continue;
+            }
+            switch(fmt[i]){
+                case 's':
+                    tmp = va_arg(ap,char*);
+                    if(fmtnum > strlen(tmp)){
+                        for(int a = 0; a < fmtnum-strlen(tmp);a++){
+                            out[j] = ' ';
+                            j++;
+                        }
+                    }
+                    strcat(out+j, tmp);
+                    j += strlen(tmp);
+                    count++;
+                    break;
+                case 'd':
+                    inte = va_arg(ap, int);
+                    k = 0;
+                    sflag = inte < 0;
+                    inte = inte *(sflag?-1:1);
+                    if(sflag){
+                        out[j] = '-';
+                        j++;
+                    }
+                    do{
+                        tmpc[k] = inte % 10;
+                        inte /= 10;
+                        k++;
+                    }while(inte);
+                    if(fmtnum > k){
+                        for(int l = 0; l < fmtnum-k;l++){
+                            out[j] = ' ';
+                            j++;
+                        }
+                    }
+                    for(int l = 0; l < k;l++){
+                        out[j+l] = tmpc[k-1-l] + '0';
+                    }
+                    j+=k;
+                    count++;
+                    break;
+                    //TODO: deal with '%'
+                default:
+                    assert(0 && "Unfinish stdio");
+            }
+            continue;
+        }
+        if(fmt[i] == '%'){
+            flag = 1;
+            fmtnum = 0;
+            continue;
+        }
+        out[j] = fmt[i];
+        j++;
+    }
+    out[j] = 0;
+    return count;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  int cnt = _print(&out, fmt, ap);
-  va_end(ap);
-  return cnt;
+    va_list ap;
+    va_start(ap, fmt);
+    int count = vsprintf(out, fmt, ap);
+    va_end(ap);
+    return count;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
