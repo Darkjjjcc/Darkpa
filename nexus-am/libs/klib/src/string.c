ca1a3bcaad5bce2af76e6fe4e6bb33694292fb23
tracer-ics2019 <tracer@njuics.org>
2024-10-05 11:08:13 +0800

    >  run U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:08:13 up  1:46,  1 user,  load average: 0.17, 0.14, 0.10 b590ec18afed490a7cd76238bccac76c34834cc3

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,79 +3,87 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  for(size_t i = 0;;i++){
-    if(s[i] == '\0') return i;
-  }
-  return 0;
+  int i=0;
+  while(s[i]!='\0')
+    i++;
+  return i;
 }
 
 char *strcpy(char* dst,const char* src) {
-  for(size_t i = 0;;i++){
-    dst[i] = src[i];
-    if(src[i] == '\0') return dst;
+  int i=0;
+  while(src[i]!='\0'){
+    dst[i]=src[i];
+    i++;
   }
+  dst[i]='\0';
   return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  for(size_t i = 0;i < n;i++){
-    dst[i] = src[i];
-    if(src[i] == '\0') return dst;
+  int i=0;
+  while(src[i]!='\0'&&i<n){
+    dst[i]=src[i];
+    i++;
+  }
+  while(i<n){
+    dst[i]='\0';
+    i++;
   }
   return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  size_t len = strlen(dst);
-  for(size_t i = 0;;i++){
-    dst[len + i] = src[i];
-    if(src[i] == '\0') return dst;
+  int i=0;
+  while(dst[i]!='\0')
+    i++;
+  int j=0;
+  while(src[j]!='\0'){
+    dst[i+j]=src[j];
+    j++;
   }
+  dst[i+j]='\0';
   return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  for(size_t i = 0;;i++){
-    if(s1[i] != s2[i]) return s1[i] - s2[i];
-    if(s1[i] == '\0') return 0;
-  }
-  return 0;
+  int i=0;
+  while(s1[i]!='\0'&&s1[i]==s2[i])
+    i++;
+  return s1[i]-s2[i];
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  for(size_t i = 0;i < n;i++){
-    if(s1[i] != s2[i]) return s1[i] - s2[i];
-    if(s1[i] == '\0') return 0;
-  }
-  return 0;
+  int i=0;
+  while(s1[i]!='\0'&&i<n&&s1[i]==s2[i])
+    i++;
+  if(i==n)
+    return 0;
+  return s1[i]-s2[i];
 }
 
 void* memset(void* v,int c,size_t n) {
-  for(size_t i = 0;i < n;i++){
-    ((char*)v)[i] = c;
+  char *temp=v;
+  for(int i=0;i<n;i++){
+    temp[i]=(char)c;
   }
   return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  for(size_t i = 0;i < n;i++){
-    ((char*)out)[i] = ((char*)in)[i];
+  char *temp=out;
+  for(int i=0;i<n;i++){
+    temp[i]=((char*)in)[i];
   }
   return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  const unsigned char *p1 = (const unsigned char *)s1;  
-  const unsigned char *p2 = (const unsigned char *)s2;  
-
-  while (n--) {
-      if (*p1 != *p2) {  
-          return (int)(*p1 - *p2);  
-      }  
-      p1++;  
-      p2++;  
-  }  
+  char *temp1=s1;
+  char *temp2=s2;
+  for(int i=0;i<n;i++){
+    if(temp1[i]!=temp2[i])
+      return temp1[i]-temp2[i];
+  }
   return 0;
 }
-
 #endif
