91034a4cd85bda2d0ef5f36f025f57fc46c613a6
tracer-ics2019 <tracer@njuics.org>
2024-10-05 10:58:49 +0800

    >  compile U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:58:49 up  1:37,  1 user,  load average: 0.04, 0.06, 0.07 71fd64e34066ee98dcdf1ec825f28e10e30452ab

--- nemu/src/isa/riscv32/decode.c
+++ nemu/src/isa/riscv32/decode.c
@@ -66,12 +66,13 @@ make_DHelper(I)
 }
 
 // pa2 added for J-type instructions
-make_DHelper(J)
-{
-  int32_t offset = decinfo.isa.instr.simm20<<20 | decinfo.isa.instr.imm10_1<<1 | decinfo.isa.instr.imm11_<<11 | decinfo.isa.instr.imm19_12<<12;
-  decode_op_i(id_src, offset, true);
-  print_Dop(id_src->str, OP_STR_SIZE, "0x%x", offset);
-  decode_op_r(id_dest, decinfo.isa.instr.rd, false);
+make_DHelper(J){
+  int32_t offset=(decinfo.isa.instr.simm20<<20)|(decinfo.isa.instr.imm19_12<<12)|(decinfo.isa.instr.imm11_<<11)|(decinfo.isa.instr.imm10_1<<1);
+  decode_op_i(id_src,offset,true);
+
+  print_Dop(id_src->str,OP_STR_SIZE,"0x%x",offset);
+
+  decode_op_r(id_dest,decinfo.isa.instr.rd,false);
 }
 
 // pa2 added for B-type instructions
