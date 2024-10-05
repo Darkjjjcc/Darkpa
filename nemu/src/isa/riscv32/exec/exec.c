3ba5cc0ca6d3bf411ffddd4d68049009f9363cd0
tracer-ics2019 <tracer@njuics.org>
2024-10-05 10:40:56 +0800

    >  compile U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:40:56 up  1:19,  1 user,  load average: 0.11, 0.16, 0.12 7ef7a0c19e76bd15dd93a4c1a19cd5a129b56716

--- nemu/src/isa/riscv32/exec/exec.c
+++ nemu/src/isa/riscv32/exec/exec.c
@@ -20,7 +20,7 @@ static make_EHelper(store) {
   // printf("now instr is %x\n", decinfo.isa.instr.val);
   // printf("at exec_store, now pc is %x\n", cpu.pc);
   printf("at exec_store, now funct3 is %d\n", decinfo.isa.instr.funct3);
-  printf("at exec_store, now store_table[decinfo.isa.instr.funct3] is %x\n", store_table[decinfo.isa.instr.funct3]);
+  printf("at exec_store, now store_table[decinfo.isa.instr.funct3] is %x\n", store_table[0]);
   idex(pc, &store_table[decinfo.isa.instr.funct3]);
 }
 
