61e2459097aeb74b4afc0791f0fd1e472bf8cba5
tracer-ics2019 <tracer@njuics.org>
2024-10-05 11:16:55 +0800

    >  compile U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:16:55 up  1:55,  1 user,  load average: 0.35, 0.23, 0.13 5598c53a393b4ae0549e3a1b82aa91266a9964f3

--- nemu/src/isa/riscv32/exec/control.c
+++ nemu/src/isa/riscv32/exec/control.c
@@ -1,57 +1,57 @@
-// #include "cpu/exec.h"
+#include "cpu/exec.h"
 
-// // pa2.1_4 jal
-// make_EHelper(jal){
-//   uint32_t addr = cpu.pc + 4;
-//   rtl_sr(id_dest->reg, &addr, 4);
+// pa2.1_4 jal
+make_EHelper(jal){
+  uint32_t addr = cpu.pc + 4;
+  rtl_sr(id_dest->reg, &addr, 4);
 
-//   rtl_add(&decinfo.jmp_pc, &cpu.pc, &id_src->val);
-//   rtl_j(decinfo.jmp_pc);
+  rtl_add(&decinfo.jmp_pc, &cpu.pc, &id_src->val);
+  rtl_j(decinfo.jmp_pc);
 
-//   print_asm_template2(jal);
-// }
+  print_asm_template2(jal);
+}
 
-// // pa2.1_5 jalr
-// make_EHelper(jalr){
-//   uint32_t addr = cpu.pc + 4;
-//   rtl_sr(id_dest->reg, &addr, 4);
+// pa2.1_5 jalr
+make_EHelper(jalr){
+  uint32_t addr = cpu.pc + 4;
+  rtl_sr(id_dest->reg, &addr, 4);
 
-//   decinfo.jmp_pc = (id_src->val+id_src2->val)&(~1);
-//   rtl_j(decinfo.jmp_pc);
+  decinfo.jmp_pc = (id_src->val+id_src2->val)&(~1);
+  rtl_j(decinfo.jmp_pc);
 
-//   print_asm_template2(jalr);
-// }
+  print_asm_template2(jalr);
+}
 
-// // PA2.2 B-type instructions
-// make_EHelper(Branch){
-//   decinfo.jmp_pc = cpu.pc + id_dest->val;
-//   switch(decinfo.isa.instr.funct3){
-//     case 0b000:     // beq||beqz
-//       rtl_jrelop(RELOP_EQ, &id_src->val, &id_src2->val, decinfo.jmp_pc);
-//       print_asm_template3(beq);
-//       break;
-//     case 0b001:     // bne||bnez
-//       rtl_jrelop(RELOP_NE, &id_src->val, &id_src2->val, decinfo.jmp_pc);
-//       print_asm_template3(bne);
-//       break;
-//     case 0b100:     // blt||bltz
-//       rtl_jrelop(RELOP_LT, &id_src->val, &id_src2->val, decinfo.jmp_pc);
-//       print_asm_template3(blt);
-//       break;
-//     case 0b101:     // bge||bgez
-//       rtl_jrelop(RELOP_GE, &id_src->val, &id_src2->val, decinfo.jmp_pc);
-//       print_asm_template3(bge);
-//       break;
-//     case 0b110:     // bltu
-//       rtl_jrelop(RELOP_LTU, &id_src->val, &id_src2->val, decinfo.jmp_pc);
-//       print_asm_template3(bltu);
-//       break;
-//     case 0b111:   // bgeu
-//       rtl_jrelop(RELOP_GEU, &id_src->val, &id_src2->val, decinfo.jmp_pc);
-//       print_asm_template3(bgeu);
-//       break;
-//     default:
-//       assert(0);
-//       break;
-//   }
-// }
+// PA2.2 B-type instructions
+make_EHelper(Branch){
+  decinfo.jmp_pc = cpu.pc + id_dest->val;
+  switch(decinfo.isa.instr.funct3){
+    case 0b000:     // beq||beqz
+      rtl_jrelop(RELOP_EQ, &id_src->val, &id_src2->val, decinfo.jmp_pc);
+      print_asm_template3(beq);
+      break;
+    case 0b001:     // bne||bnez
+      rtl_jrelop(RELOP_NE, &id_src->val, &id_src2->val, decinfo.jmp_pc);
+      print_asm_template3(bne);
+      break;
+    case 0b100:     // blt||bltz
+      rtl_jrelop(RELOP_LT, &id_src->val, &id_src2->val, decinfo.jmp_pc);
+      print_asm_template3(blt);
+      break;
+    case 0b101:     // bge||bgez
+      rtl_jrelop(RELOP_GE, &id_src->val, &id_src2->val, decinfo.jmp_pc);
+      print_asm_template3(bge);
+      break;
+    case 0b110:     // bltu
+      rtl_jrelop(RELOP_LTU, &id_src->val, &id_src2->val, decinfo.jmp_pc);
+      print_asm_template3(bltu);
+      break;
+    case 0b111:   // bgeu
+      rtl_jrelop(RELOP_GEU, &id_src->val, &id_src2->val, decinfo.jmp_pc);
+      print_asm_template3(bgeu);
+      break;
+    default:
+      assert(0);
+      break;
+  }
+}
