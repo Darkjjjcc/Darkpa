2caad8496d1136bf98efe84e79cefc1888ba890f
tracer-ics2019 <tracer@njuics.org>
2024-10-06 15:11:27 +0800

    >  run U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:11:27 up  6:06,  1 user,  load average: 0.20, 0.29, 0.24 96643a77b9f42153c4b4220b30addeac2fa82989

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -67,30 +67,25 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
         }
         case 'x':{
           int n=va_arg(ap,int);
-          if(n==0){
-            *temp++='0';
-            *temp++='x';
-            *temp++='0';
-            break;
-          }
-          if(n<0){
-            *temp++='-';
-            n=-n;
+          if(n==0) add_string(&temp,"0x0");break;
+          if (n < 0) {
+            *temp++ = '-';
+            n = -n;
           }
           char buf[12];
-          int i=0;
-          while(n!=0){
-            int a=n%16;
-            if(a<10)
-              buf[i++]=a+'0';
+          int i = 0;
+          while (n != 0) {
+            int a = n % 16;
+            if (a < 10)
+              buf[i++] = a + '0';
             else
-              buf[i++]=a-10+'a';
-            n=n/16;
+              buf[i++] = a - 10 + 'a';
+            n = n / 16;
           }
-          *temp++='0';
-          *temp++='x';
-          for(int j=i-1;j>=0;j--){
-            *temp++=buf[j];
+          *temp++ = '0';
+          *temp++ = 'x';
+          for (int j = i - 1; j >= 0; j--) {
+            *temp++ = buf[j];
           }
           break;
         }
