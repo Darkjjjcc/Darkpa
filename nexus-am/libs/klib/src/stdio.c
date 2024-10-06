06a0248d0ffbc6092c466efb178a55ae0144b713
tracer-ics2019 <tracer@njuics.org>
2024-10-06 11:37:15 +0800

    >  run U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:37:15 up  2:32,  1 user,  load average: 0.04, 0.16, 0.18 cf5bf60d1ef9a265e96863242512012eb61b2eae

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,123 +3,314 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-const char lower_dict[] = "0123456789abcdef";
-const char upper_dict[] = "0123456789ABCDEF";
+#define bool int
+#define true 1
+#define false 0
+#define ZEROPAD 1  // Pad with zero
+#define SIGN 2     // Unsigned/signed long
+#define PLUS 4     // Show plus
+#define SPACE 8    // Space if plus
+#define LEFT 16    // Left justified
+#define SPECIAL 32 // 0x
+#define LARGE 64   // Use 'ABCD' instead of 'abcd'
 
-// 借鉴了 https://github.com/atgreen/FreeRTOS/blob/master/Demo/CORTEX_STM32F103_Primer_GCC/printf-stdarg.c
-static inline int _printc(char **out, char c) {
-  if (out) {
-    **out = c;
-    (*out)++;
-  } else {
-    _putc(c);
+static char *digits = "0123456789abcdefghijklmnopqrstuvwxyz";
+static char *upper_digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+static inline int is_space(int ch) { return (unsigned long)(ch - 9) < 5u || ' ' == ch; }
+static inline int is_digit(int ch) { return ch >= '0' && ch <= '9'; }
+
+static int skip_atoi(const char **s) {
+  int i = 0;
+  while (is_digit(**s)) {
+    i = i * 10 + *((*s)++) - '0';
   }
-  return 1;
+  return i;
 }
 
-static inline int _prints(char **out, const char *s) {
-  int cnt = 0;
-  while (*s != '\0') {
-    cnt += _printc(out, *(s++));
-  }
-  return cnt;
-}
+static char *number(char *str, long num, int base, int size, int precision, int type) {
+  char c, sign, tmp[66];
+  char *dig = digits;
+  int i;
 
-static inline int _printi(char **out, int num, int base, int use_upper, int sign) {
-  assert(base <= 16 && base >= 2);
+  if (type & LARGE)
+    dig = upper_digits;
+  if (type & LEFT)
+    type &= ~ZEROPAD;
+  if (base < 2 || base > 36)
+    return 0;
 
-  const int buf_sz = 51;
-  char buf[buf_sz];
-  buf[buf_sz - 1] = '\0';
-  const char *dict = use_upper ? upper_dict : lower_dict;
+  c = (type & ZEROPAD) ? '0' : ' ';
 
-  if (num == 0) {
-    return _printc(out, dict[0]);
-  }
-
-  char *p = &buf[buf_sz - 1];
-  if (!sign) {
-    unsigned int n = *(unsigned int *)&num;
-    while (n != 0) {
-      p--;
-      *p = dict[n % base];
-      n /= base;
-    }
-  } else {
-    int is_neg = 0;
+  sign = 0;
+  if (type & SIGN) {
     if (num < 0) {
+      sign = '-';
       num = -num;
-      is_neg = 1;
+      size--;
     }
+    else if (type & PLUS) {
+      sign = '+';
+      size--;
+    }
+    else if (type & SPACE) {
+      sign = ' ';
+      size--;
+    }
+  }
 
+  if (type & SPECIAL) {
+    if (16 == base)
+      size -= 2;
+    else if (8 == base)
+      size--;
+  }
+
+  i = 0;
+
+  if (0 == num) {
+    tmp[i++] = '0';
+  }
+  else {
     while (num != 0) {
-      p--;
-      *p = dict[num % base];
-      num /= base;
-    }
-
-    if (is_neg) {
-      p--;
-      *p = '-';
+      tmp[i++] = dig[((unsigned long)num) % (unsigned)base];
+      if (base == 16)
+        num = (unsigned)num >> 4;
+      else
+        num = ((unsigned long)num) / (unsigned)base;
     }
   }
 
-  return _prints(out, p);
+  if (i > precision)
+    precision = i;
+  size -= precision;
+  if (!(type & (ZEROPAD | LEFT))) {
+    while (size-- > 0)
+      *str++ = ' ';
+  }
+  if (sign)
+    *str++ = sign;
+
+  if (type & SPECIAL) {
+    if (8 == base) {
+      *str++ = '0';
+    }
+    else if (16 == base) {
+      *str++ = '0';
+      *str++ = digits[33];
+    }
+  }
+
+  if (!(type & LEFT)) {
+    while (size-- > 0)
+      *str++ = c;
+  }
+  while (i < precision--)
+    *str++ = '0';
+  while (i-- > 0)
+    *str++ = tmp[i];
+  while (size-- > 0)
+    *str++ = ' ';
+
+  return str;
 }
 
-static inline int _print(char **out, const char *fmt, va_list ap) {
-  const char *p = fmt;
-  int cnt = 0;
-  while (*p) {
-    if (*p == '%') {
-      p++;
-      switch (*p) {
-        case 'd':
-          cnt += _printi(out, va_arg(ap, int), 10, 0, 1);
-          break;
-        case 'u':
-          cnt += _printi(out, va_arg(ap, int), 10, 0, 0);
-          break;
-        case 'x':
-          cnt += _printi(out, va_arg(ap, int), 16, 0, 0);
-          break;
-        case 'X':
-          cnt += _printi(out, va_arg(ap, int), 16, 1, 0);
-          break;
-        case 's':
-          cnt += _prints(out, va_arg(ap, char *));
-          break;
-        default:
-          break;
+int printf(const char *fmt, ...)
+{
+  char buf[128] = {'\0'};
+  va_list args;
+  int n;
+
+  va_start(args, fmt);
+  n = vsprintf(buf, fmt, args);
+  for (size_t i = 0; i < 128; i++)
+  {
+    if (buf[i] == '\0')
+      break;
+    else
+      _putc(buf[i]);
+  }
+  va_end(args);
+  return n;
+  return 0;
+}
+
+int vsprintf(char *buf, const char *fmt, va_list args)
+{
+
+  char *str;
+  int field_width; /*width of output field*/
+
+  for (str = buf; *fmt; fmt++)
+  {
+    unsigned long num;
+    int base = 10;
+    int flags = 0;
+    int qualifier = -1;
+    int precision = -1;
+    bool bFmt = true;
+    if (*fmt != '%')
+    {
+      *str++ = *fmt;
+      continue;
+    }
+
+    bFmt = true;
+    while (bFmt)
+    {
+      fmt++; /*This also skips first '%'*/
+      switch (*fmt)
+      {
+      case '0':
+        flags |= ZEROPAD;
+        break;
+      case '-':
+      case '+':
+      case ' ':
+      case '#':
+        _putc('$');
+        break;
+      default:
+        bFmt = false;
+        break;
       }
-    } else {
-      cnt += _printc(out, *p);
     }
-    p++;
-  }
-  if (out)
-    **out = '\0';
-  return cnt;
+
+    /*Get field width*/
+    field_width = -1;
+    if (is_digit(*fmt))
+    {
+      field_width = skip_atoi(&fmt);
+    }
+    else if ('*' == *fmt)
+    {
+      fmt++;
+      field_width = va_arg(args, int);
+      if (field_width < 0)
+      {
+        field_width = -field_width;
+        flags |= LEFT;
+      }
+    }
+
+    /*Get the precision*/
+    precision = -1;
+    if ('.' == *fmt)
+    {
+      _putc('$');
+    }
+    /*Get the conversion qualifier*/
+    qualifier = -1;
+    if ('h' == *fmt || 'l' == *fmt || 'L' == *fmt)
+    {
+      qualifier = *fmt;
+      fmt++;
+    }
+
+    /*Default base*/
+    base = 10;
+    switch (*fmt)
+    {
+    case 'c':
+    {
+      if (!(flags & LEFT))
+        while (--field_width > 0)
+          *str++ = ' ';
+      *str++ = (unsigned char)va_arg(args, int);
+      while (--field_width > 0)
+        *str++ = ' ';
+      continue;
+    }
+    case 's':
+    {
+      int len;
+      char *s = va_arg(args, char *);
+      if (!s)
+        s = "<NULL>";
+      len = strlen(s);
+      if (!(flags & LEFT))
+        while (len < field_width--)
+          *str++ = ' ';
+      for (int i = 0; i < len; ++i)
+        *str++ = *s++;
+      while (len < field_width--)
+        *str++ = ' ';
+      continue;
+    }
+    case 'p':
+    case 'n':
+    case 'A':
+    case 'a':
+    case 'o':
+    case 'X':
+      _putc('$');
+      break;
+    case 'x':
+    {
+      base = 16;
+      break;
+    }
+    case 'd':
+    case 'i':
+    {
+      flags |= SIGN; /*no break*/
+    }
+    case 'u':
+    {
+      break;
+    }
+    default:
+    {
+      if (*fmt != '%')
+        *str++ = '%';
+
+      if (*fmt)
+      {
+        *str++ = *fmt;
+      }
+      else
+      {
+        --fmt;
+      }
+      continue;
+    }
+    } /*end of switch(*fmt)*/
+
+    if (qualifier == 'l')
+    {
+      num = va_arg(args, unsigned long);
+    }
+    else if (qualifier == 'h')
+    {
+      if (flags & SIGN)
+        num = va_arg(args, int);
+      else
+        num = va_arg(args, unsigned);
+    }
+    else if (flags & SIGN)
+    {
+      num = va_arg(args, int);
+    }
+    else
+    {
+      num = va_arg(args, unsigned long);
+    }
+
+    str = number(str, num, base, field_width, precision, flags);
+  } 
+  *str = '\0';
+  return str - buf;
 }
 
-int printf(const char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  int cnt = _print(NULL, fmt, ap);
-  va_end(ap);
-  return cnt;
-}
+int sprintf(char *buf, const char *fmt, ...) {
+  va_list args;
+  int n;
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  return _print(&out, fmt, ap);
-}
+  va_start(args, fmt);
+  n = vsprintf(buf, fmt, args);
+  va_end(args);
 
-int sprintf(char *out, const char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  int cnt = _print(&out, fmt, ap);
-  va_end(ap);
-  return cnt;
+  return n;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
