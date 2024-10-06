37ce69be7e1ca91b9eb37bc7804bf0c9ef8648d1
tracer-ics2019 <tracer@njuics.org>
2024-10-06 11:22:36 +0800

    >  run U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:22:36 up  2:17,  1 user,  load average: 0.22, 0.20, 0.12 2e46f308c4a1207c837b444551b6862c7d3ee401

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -8,9 +8,10 @@ size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _DEV_TIMER_UPTIME_t *uptime = (_DEV_TIMER_UPTIME_t *)buf;
-      uint32_t current_time = inl(RTC_ADDR);
+      // uint32_t current_time = inl(RTC_ADDR);
       uptime->hi = 0;
-      uptime->lo = current_time - boot_time;
+      // uptime->lo = current_time - boot_time;
+      uptime->lo = 0;
       return sizeof(_DEV_TIMER_UPTIME_t);
     }
     case _DEVREG_TIMER_DATE: {
@@ -28,5 +29,5 @@ size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
 }
 
 void __am_timer_init() {
-  boot_time = inl(RTC_ADDR);
+  // boot_time = inl(RTC_ADDR);
 }
