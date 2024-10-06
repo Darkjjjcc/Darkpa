52351c39db0c22c2f44111599caeefd873c7a44c
tracer-ics2019 <tracer@njuics.org>
2024-10-06 12:11:52 +0800

    >  run U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:11:52 up  3:07,  1 user,  load average: 0.34, 0.21, 0.14 5ec7af6c340c8e6a973ae3e94538466e4fff662a

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -47,77 +47,31 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  char *temp=out;
-  while(*fmt!='\0'){
-    if(*fmt!='%'){
-      *temp++=*fmt++;
-    }
-    else{
+  char *s = out;
+  int len = 0;
+  while(*fmt) {
+    if(*fmt == '%') {
+      *s++ = *fmt++;
       fmt++;
-      switch(*fmt){
-        case 's':{
-          char *str=va_arg(ap,char*);
-          while(*str!='\0'){
-            *temp++=*str++;
-          }
+      switch(*fmt) {
+        case 's': {
+          char *str = va_arg(ap, char *);
+          add_string(s, str);
           break;
         }
-        case 'd':{
-          int n=va_arg(ap,int);
-          if(n==0){
-            *temp++='0';
-            break;
-          }
-          if(n<0){
-            *temp++='-';
-            n=-n;
-          }
-          char buf[11];
-          int i=0;
-          while(n!=0){
-            buf[i++]=n%10+'0';
-            n=n/10;
-          }
-          for(int j=i-1;j>=0;j--){
-            *temp++=buf[j];
-          }
-          break;
-        }
-        case 'x':{
-          int n=va_arg(ap,int);
-          if(n==0){
-            *temp++='0';
-            *temp++='x';
-            *temp++='0';
-            break;
-          }
-          if(n<0){
-            *temp++='-';
-            n=-n;
-          }
-          char buf[12];
-          int i=0;
-          while(n!=0){
-            int a=n%16;
-            if(a<10)
-              buf[i++]=a+'0';
-            else
-              buf[i++]=a-10+'a';
-            n=n/16;
-          }
-          *temp++='0';
-          *temp++='x';
-          for(int j=i-1;j>=0;j--){
-            *temp++=buf[j];
-          }
+        case 'd': {
+          int num = va_arg(ap, int);
+          add_number(s, num);
           break;
         }
       }
-      fmt++;
+    } 
+    else {
+      add_char(s, *fmt);
     }
+    fmt++;
   }
-  *temp='\0';
-  return temp-out;
+  return s - out;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
