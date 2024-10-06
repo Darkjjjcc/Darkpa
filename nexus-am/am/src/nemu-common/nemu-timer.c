e1b8b5e5186c8b5a631e63b8c87f7131045b2435
tracer-ics2019 <tracer@njuics.org>
2024-10-06 11:18:39 +0800

    >  run U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:18:39 up  2:13,  1 user,  load average: 0.19, 0.15, 0.10 8a0697dbcc0b6dc42ee2ab18b0d2c94ca078867b

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -11,14 +11,14 @@ size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
       // PA2.3 TODO: calculate time using RTC (Real Time Counter)
       uint32_t current_time = inl(RTC_ADDR);
       uptime->hi = 0;
-      uptime->lo = current_time - boot_time;
+      uptime->lo = current_time;
       return sizeof(_DEV_TIMER_UPTIME_t);
     }
     case _DEVREG_TIMER_DATE: {
       _DEV_TIMER_DATE_t *rtc = (_DEV_TIMER_DATE_t *)buf;
-      rtc->second = 2;
-      rtc->minute = 2;
-      rtc->hour   = 2;
+      rtc->second = 0;
+      rtc->minute = 0;
+      rtc->hour   = 0;
       rtc->day    = 0;
       rtc->month  = 0;
       rtc->year   = 2000;
