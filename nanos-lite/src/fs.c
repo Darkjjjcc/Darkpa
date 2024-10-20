c2bde3720afe80e05b12fbbeaade6a27a5a840e8
tracer-ics2019 <tracer@njuics.org>
2024-10-20 17:24:30 +0800

    >  run U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:24:30 up 1 day, 17:55,  1 user,  load average: 0.25, 0.24, 0.17 7aa8ece4a5dbb05ebd3702570fba0468238d6c2

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -51,7 +51,7 @@ int fs_open(const char *pathname, int flags, int mode){
 }
 
 size_t fs_read(int fd, void *buf, size_t len) {  
-  printf("open_offset = %d, disk_offset = %d\n", file_table[fd].open_offset, file_table[fd].disk_offset);
+  // printf("open_offset = %d, disk_offset = %d\n", file_table[fd].open_offset, file_table[fd].disk_offset);
   assert(fd >= 3 && fd < NR_FILES);
   if (file_table[fd].open_offset + len >= file_table[fd].size) {
     if (file_table[fd].size > file_table[fd].open_offset)
@@ -59,7 +59,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
     else
       len = 0;
   }
-  printf("read %d bytes\n", len);
+  // printf("read %d bytes\n", len);
   if (!file_table[fd].read) {
     ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
   }
@@ -85,7 +85,7 @@ size_t fs_write(int fd, const void *buf, size_t len) {
     else
       len = 0;
   }
-  // printf("write %d bytes\n", len);
+  printf("write %d bytes\n", len);
   if (!file_table[fd].write) {
     ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
   }
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -78,7 +78,7 @@ int sys_write(int fd, void *buf, size_t count) {
 }
 
 int sys_brk(intptr_t addr) {
-  printf("brk %x\n", addr);
+  // printf("brk %x\n", addr);
   program_break = addr;
   return 0;
 }
