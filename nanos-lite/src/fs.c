0706c6a64548016cbea4a8714ca9d4ed5a0511fe
tracer-ics2019 <tracer@njuics.org>
2024-10-20 17:19:45 +0800

    >  run U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:19:45 up 1 day, 17:50,  1 user,  load average: 0.32, 0.19, 0.14 99131794b5e8904d1d7aa824fa1e7c303ef6313a

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -67,7 +67,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
     len = file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
   }
   file_table[fd].open_offset += len;
-//   printf("open_offset = %d, disk_offset = %d\n", file_table[fd].open_offset, file_table[fd].disk_offset);
+  printf("open_offset = %d, disk_offset = %d\n", file_table[fd].open_offset, file_table[fd].disk_offset);
   return len;
 }
 
