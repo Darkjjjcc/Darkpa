ee212f2ba5f12eade08687b0f8d35f92a5c40d0a
tracer-ics2019 <tracer@njuics.org>
2024-10-20 17:17:22 +0800

    >  run U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:17:22 up 1 day, 17:47,  1 user,  load average: 0.10, 0.11, 0.11 3362b2b9d5956dae423f5fada58d22f118319492

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -40,29 +40,35 @@ static Finfo file_table[] __attribute__((used)) = {
 
 // PA3.3 updated
 int fs_open(const char *pathname, int flags, int mode){
-    for(int i = 3; i < NR_FILES;i++){
-        if(strcmp(pathname, file_table[i].name) == 0){
-            return i;
-        }
+  // printf("open %s\n", pathname);
+  for(int i = 0; i < NR_FILES;i++){
+    if(strcmp(pathname, file_table[i].name) == 0){
+        // printf("open %s OK\n", pathname);
+        return i;
     }
-    assert(0 && "Can't find file");
+  }
+  assert(0 && "Can't find file");
 }
 
 size_t fs_read(int fd, void *buf, size_t len) {  
-    if(fd>=3 &&(file_table[fd].open_offset+len >= file_table[fd].size)){
-        if(file_table[fd].size > file_table[fd].open_offset)
-            len = file_table[fd].size - file_table[fd].open_offset;
-        else
-            len = 0;
-    }
-    if(!file_table[fd].read){
-        ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-    }
-    else{
-        len = file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-    }
-    file_table[fd].open_offset += len;
-    return len;
+//   printf("open_offset = %d, disk_offset = %d\n", file_table[fd].open_offset, file_table[fd].disk_offset);
+  assert(fd >= 3 && fd < NR_FILES);
+  if (file_table[fd].open_offset + len >= file_table[fd].size) {
+    if (file_table[fd].size > file_table[fd].open_offset)
+      len = file_table[fd].size - file_table[fd].open_offset;
+    else
+      len = 0;
+  }
+//   printf("read %d bytes\n", len);
+  if (!file_table[fd].read) {
+    ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  }
+  else {
+    len = file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  }
+  file_table[fd].open_offset += len;
+//   printf("open_offset = %d, disk_offset = %d\n", file_table[fd].open_offset, file_table[fd].disk_offset);
+  return len;
 }
 
 size_t fs_write(int fd, const void *buf, size_t len) {
@@ -73,35 +79,33 @@ size_t fs_write(int fd, const void *buf, size_t len) {
     }
     return len;
   }
-    if(fd>=5 &&(file_table[fd].open_offset+len > file_table[fd].size)){
-        if(file_table[fd].size > file_table[fd].open_offset)
-            len = file_table[fd].size - file_table[fd].open_offset;
-        else
-            len = 0;
-    }
-    if(!file_table[fd].write){
-        ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-    }
-    else{
-        file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-    }
-    file_table[fd].open_offset += len;
-    return len;
+  if (fd >= 3 && (file_table[fd].open_offset + len > file_table[fd].size)) {
+    if (file_table[fd].size > file_table[fd].open_offset)
+      len = file_table[fd].size - file_table[fd].open_offset;
+    else
+      len = 0;
+  }
+  // printf("write %d bytes\n", len);
+  if (!file_table[fd].write) {
+    ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  }
+  else {
+    file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  }
+  file_table[fd].open_offset += len;
+  return len;
 }
 
-size_t fs_lseek(int fd, size_t offset, int whence){
-    switch(whence){
-        case SEEK_SET:
-            file_table[fd].open_offset = offset;
-            break;
-        case SEEK_CUR:
-            file_table[fd].open_offset += offset;
-            break;
-        case SEEK_END:
-            file_table[fd].open_offset = file_table[fd].size + offset;
-            break;
-    }
-    return file_table[fd].open_offset;
+size_t fs_lseek(int fd, size_t offset, int whence) {
+  // printf("lseek %d %d %d\n", fd, offset, whence);
+  assert(fd >= 3 && fd < NR_FILES);
+  switch(whence) {
+    case SEEK_SET: file_table[fd].open_offset = offset; break;
+    case SEEK_CUR: file_table[fd].open_offset += offset; break;
+    case SEEK_END: file_table[fd].open_offset = file_table[fd].size + offset; break;
+    default: assert(0 && "Invalid whence");
+  }
+  return file_table[fd].open_offset;
 }
 
 int fs_close(int fd){
