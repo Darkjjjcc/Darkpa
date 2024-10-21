788a90c0a51187bd5c52efb20d458fdd5a784e12
tracer-ics2019 <tracer@njuics.org>
2024-10-21 10:47:33 +0800

    >  run U202115453 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:47:33 up 14 min,  1 user,  load average: 0.58, 0.97, 0.83 afa43c74ea5e7d44e7ba118a38dc320d2b0513da

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,13 +1,13 @@
 #include "common.h"
 #include <amdev.h>
 
+extern int screen_width();
+extern int screen_height();
+
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-#ifdef MULTI_PROC
-  _yield();
-#endif
-  int i;
-  const char* s = (char*) buf;
-  for(i=0;i<len;++i)_putc(s[i]);
+  for (size_t i = 0; i < len; i++) {
+    _putc(((char *)buf)[i]);
+  }
   return 0;
 }
 
@@ -20,90 +20,39 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-#ifdef MULTI_PROC
-  _yield();
-#endif
-  _DEV_INPUT_KBD_t key;
-  _io_read(_DEV_INPUT, _DEVREG_INPUT_KBD, &key, sizeof(_DEV_INPUT_KBD_t));
-  if (key.keycode){
-    if(key.keydown){
-      sprintf(buf,"kd %s\n",keyname[0x7fff&key.keycode]);
-    }else{
-      sprintf(buf,"ku %s\n",keyname[0x7fff&key.keycode]);
-    }
-  } else {
-    _DEV_TIMER_UPTIME_t rtc;
-    _io_read(_DEV_TIMER, _DEVREG_TIMER_UPTIME, &rtc, sizeof(_DEV_TIMER_UPTIME_t));
-    sprintf(buf,"t %d\n",rtc.lo);
+  int kc = read_key();
+  char tmp[3] = "ku";
+  if ((kc & 0xfff) == _KEY_NONE) {
+    int time = uptime();
+    len = sprintf(buf, "t %d\n", time);
   }
-  len = strlen(buf);
+  else {
+    if (kc & 0x8000)
+      tmp[1] = 'd';
+    len = sprintf(buf, "%s %s\n", tmp, keyname[kc & 0xfff]);
+  }
   return len;
 }
 
 static char dispinfo[128] __attribute__((used)) = {};
-static int W,H;
-#define max(a,b) ((a<b)?b:a)
-#define min(a,b) ((a<b)?a:b)
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  int i;
-  char* s = (char*)buf;
-  int _len = max(0,min(strlen(dispinfo)-offset,len));
-  for(i=0;i<_len;++i)s[i]=dispinfo[i+offset];
-  return _len;
+  len = sprintf(buf, dispinfo + offset);
+  return len;
 }
 
-size_t fb_write(void *buf, size_t offset, size_t len) {
-#ifdef MULTIPROC
-  _yield();
-#endif
-  offset /=4;
-  //printf("fb write buf:0x%x offset:%d len:%d\n",buf,offset,len);
-  uint32_t * p=buf;
-  len = min(H*W-offset,len);
-  int ret = len;
-  _DEV_VIDEO_FBCTL_t ctl ;
-  ctl.pixels = p;
-  ctl.x = offset%W;
-  ctl.y =offset/W;
-  ctl.w = W-offset%W;
-  ctl.h = 1,
-  ctl.sync = 0,
-  _io_write(_DEV_VIDEO, _DEVREG_VIDEO_FBCTL, &ctl, sizeof(ctl));
-  
-  ctl.x=0;
-  ctl.y+=ctl.h;
-  len-=ctl.w*ctl.h;
-  p+=ctl.w*ctl.h;
-  if(len/W){
-    ctl.h=len/W;
-    ctl.w =W;
-    ctl.pixels=p;
-    _io_write(_DEV_VIDEO, _DEVREG_VIDEO_FBCTL, &ctl, sizeof(ctl));
-    ctl.y+=ctl.h;
-    len-=ctl.h*ctl.w;
-    p+=ctl.h*ctl.w;
-  }
-
-  if(len){
-    ctl.pixels = p;
-    ctl.w = len;
-    ctl.h = 1;
-    _io_write(_DEV_VIDEO, _DEVREG_VIDEO_FBCTL, &ctl, sizeof(ctl));
-  }
-  return ret;
+size_t fb_write(const void *buf, size_t offset, size_t len) {
+  int x = (offset/4) % screen_width();
+  int y = (offset/4) / screen_width();
+  draw_sync();
+  draw_rect((uint32_t*)buf, x, y, len/4, 1);
+  return len;
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {
-  //printf("fbsync write buf:0x%x offset:%d len:%d\n",buf,offset,len);
-  _DEV_VIDEO_FBCTL_t ctl;
-  ctl.pixels = NULL;
-  ctl.x = ctl.y = ctl.w = ctl.h = 0;
-  ctl.sync = 1;
-  _io_write(_DEV_VIDEO, _DEVREG_VIDEO_FBCTL, &ctl, sizeof(ctl));
+  draw_sync();
   return 0;
 }
-
 
 void init_device() {
   Log("Initializing devices...");
@@ -111,9 +60,5 @@ void init_device() {
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
-  _DEV_VIDEO_INFO_t info;
-  _io_read(_DEV_VIDEO, _DEVREG_VIDEO_INFO, &info, sizeof(info));
-  H = info.height;
-  W = info.width;
-  sprintf(dispinfo,"WIDTH:%d\nHEIGHT:%d\n",info.width,info.height);
+  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", screen_width(), screen_height());
 }
